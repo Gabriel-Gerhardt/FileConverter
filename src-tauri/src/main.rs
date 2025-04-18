bd8d37efb0293a7f430472aa2311f7fcd8d2a106
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-04-18 19:29:39 -0300

    a
    a

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod file_convert;
-use file_convert::pages_to_pdf; 
+use file_convert::pages_to_pdf;
 mod hello;
 use hello::greet;
 fn main() {
-    //let input_path = "/Users/gabrielgerhardt/downloads/atividades_algebra.pages";
-   // let output_path = "/Users/gabrielgerhardt/documents/test";
+    let input_path = "/Users/gabrielgerhardt/Downloads/GabrielGerhardt_Backend_Curriculum.pages";
+    let output_path = "/Users/gabrielgerhardt/documents/arquivos_pdf";
 
-   // if let Err(e) = pages_to_pdf(input_path, output_path) {
-       // eprintln!("Failed to convert .pages file to PDF: {}", e);
-    
-    fileconverter_lib::run()
+    if let Err(e) = pages_to_pdf(input_path, output_path) {
+        eprintln!("Failed to convert .pages file to PDF: {}", e);
+    }
+    //fileconverter_lib::run()
 }
